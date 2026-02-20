ef8433b530bb67750ea4ca754b61c1e9fac9766d
divyapardeshi <divyapradeshi@gmail.com>
2026-02-20 22:26:21 +0530

    commit

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Link as LinkScroll, Element } from "react-scroll";
+
+import Button from '../components/Button.jsx';
+const Hero = () => {
+  return (
+    <section className='relative 
+    max-lg:pt-52 
+    max-md:pt-36
+    max-md:pb-32
+    max-lg:pb-36
+    pt-60 pb-40
+    '>
+  <Element name="hero">
+    <div className='container'>
+     <div className='relative z-10
+     max-w-512 max-lg:max-w-388'>
+  <div className="caption small-2 uppercase text-p3">
+    Video Editing
+  </div>
+  <h1 className="mb-6 text-p4 uppercase max-lg:mb-7 
+   max-md:mb-4 max-md:text-5xl max-md:leading-12 max-lg:h2 h1">
+    Amazingly simple
+  </h1>
+  <p className='max-w-440 mb-14 body-1 max-md:mb-10' > We desingned XORA AI Video Editor
+     to be an easy to use, quick to learn, and surprisingly powerful. </p>
+     <LinkScroll to="features" offset={-100} spy smooth>
+     <Button icon='/images/zap.svg'>
+     Try it now
+     </Button>
+     </LinkScroll>
+     </div>
+     <div className="absolute -top-32 left-[calc(50%-340px)] w-[1230px]
+     pointer-events-none  hero-img-res">
+     <img src="/images/hero.png" className="size-1230 max-lg:h-auto"
+     alt="hero"/>
+     </div>
+    </div>
+  </Element>
+    </section>
+  )
+}
+
+export default Hero
